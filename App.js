b86c616b3dca782b53c30ecdaca02ef5e5a95288
6210110652 <6210110652@psu.ac.th>
2021-07-12 10:09:25 +0700

    90110

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Weather  from './components/Weather';
+
+
+
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
- }
- return (
- <View style={styles.container}>
- <Text onPress={doIt}>Hello world</Text>
- <StatusBar style="auto" />
- </View>
+  return (
+      <View style={styles.container}>
+          <Weather zipCode="90110"/>
+          <StatusBar style="auto" />
+      </View>
  );
 }
+
+
 const styles = StyleSheet.create({
- container: {
- flex: 1,
- backgroundColor: '#fff',
- alignItems: 'center',
- justifyContent: 'center',
- },
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
 });
