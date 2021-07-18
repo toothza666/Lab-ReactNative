a8ef65c75e06031eab2c67a5f37c3769c388a3dc
6210110652 <6210110652@psu.ac.th>
2021-07-18 10:38:23 +0700

    Temp

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function Forecast(props){
     return( <View >
@@ -11,3 +11,38 @@ export default function Forecast(props){
     
     )
 }
+
+const styles = StyleSheet.create({
+    textMain: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'rgb(255,255,255)',
+        fontSize: 36,
+        margin: 10
+    },
+    textDescription: {
+        textAlign: 'center',
+        color: 'rgb(255,255,255)',
+        fontSize: 30,
+        margin: 10
+    },
+    temp: {
+        textAlign: 'center',
+        color: 'rgb(255,255,255)',
+        fontWeight: 'bold',
+        fontSize: 36,
+        margin: 10
+    },
+    oc: {
+        textAlign: 'center',
+        color: 'rgb(255,255,255)',
+        fontWeight: 'normal',
+        fontSize: 28
+    },
+    view: {
+        flexDirection: 'row',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
--- components/Weather.js
+++ components/Weather.js
@@ -1,8 +1,29 @@
 import React, { useState } from "react"
 import { ImageBackground, Text, StyleSheet,View } from 'react-native'
 import Forecast from "./Forecast"
+import { useEffect } from "react"
 
 export default function Weather(props) {
+    
+    
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=28c099ab9b2578c63715272399321025`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+            }
+        }, [props.zipCode])
+
     const [forecastInfo, setForecastInfo] = useState({
         main : '-',
         deecription : '-',
@@ -11,9 +32,10 @@ export default function Weather(props) {
     return (
         <View>
             <ImageBackground source={require('../background.jpg')} style={styles.backdrop}>
-                <Text>Zip Code</Text>
-                <Text>{props.zipCode}</Text>
+                <View style={styles.greenBack}>
+                <Text style={styles.msg}>Zip Code {props.zipCode}</Text>
                 <Forecast {...forecastInfo} />
+                </View>
             </ImageBackground>
         </View>
  );
@@ -25,5 +47,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: '100%',
         height: '100%'
-    }
-});
+    },
+    greenBack: {
+        backgroundColor: 'rgba(0,100,0,0.1)',
+        height: '20%',
+        width: '100%'
+    },
+    msg: {
+        paddingTop: 20,
+        color: 'rgb(255,255,255)',
+        textAlign: 'center',
+        fontSize: 20
+    },
+})
